136a8f0c2f6d0f0e67f6a276895626260d59239f
BogdanPredica <predicabogdan19@stud.ase.ro>
2022-03-14 10:56:46 +0200

    sfarsitul sem 3

--- CTS_sem2/src/clase/Elev.java
+++ CTS_sem2/src/clase/Elev.java
@@ -4,16 +4,8 @@ public class Elev extends Aplicant {
     private int clasa;
     private String tutore;
 
-    public int getClasa() {
-        return clasa;
-    }
-
-    public void setClasa(int i) {
-        this.clasa = i;
-    }
-
-    public String getTutore() {
-        return tutore;
+    public void setClasa(int clasa) {
+        this.clasa = clasa;
     }
 
     public void setTutore(String tutore) {
@@ -28,13 +20,6 @@ public class Elev extends Aplicant {
 
     public Elev() {
         super();
-    }
-
-    public Elev(String nume, String prenume, int varsta, int punctaj,
-                int nr_proiecte, String[] denumireProiect, int clasa, String tutore) {
-        super(nume, prenume, varsta, punctaj, nr_proiecte, denumireProiect);
-        this.clasa = clasa;
-        this.tutore = tutore;
     }
 
 }

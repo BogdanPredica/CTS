8897ea4ff19bf678d3a782bfeffcadcb13a38d40
BogdanPredica <predica_bogdan@yahoo.com>
2022-04-04 16:37:42 +0300

    sem 4

---
+++ CTS_sem4_part2/src/main/Program.java
@@ -0,0 +1,22 @@
+package main;
+
+import clase.Autobuz;
+import clase.AutobuzBuilder;
+
+public class Program {
+    public static void main(String[] args) {
+        AutobuzBuilder autobuzBuilder = new AutobuzBuilder();
+        autobuzBuilder.setNumeSofer("Dorel").setDeschideUsile(false);
+        Autobuz autobuz = autobuzBuilder.build();
+
+        Autobuz autobuz2 = new AutobuzBuilder().setNumeSofer("Bobita").setNrLinie(5).build();
+
+        System.out.println(autobuz.toString());
+        System.out.println(autobuz2.toString());
+
+        AutobuzBuilder builder = new AutobuzBuilder();
+        builder.setTextDerulat("La multi ani").setOraIncepereProgram(9);
+
+        Autobuz a1 = builder.build();
+    }
+}
